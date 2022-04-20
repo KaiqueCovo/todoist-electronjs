4f88de9db12675b7e6a7eb62ed18fde5af276869
Kaique Covo <kaique.kng@gmail.com>
2022-04-19 22:37:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron")
+
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+function criarJanela() {
+  const janela = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: `${__dirname}/assets/icon.png`
+  })
+
+  janela.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  criarJanela()
+})
