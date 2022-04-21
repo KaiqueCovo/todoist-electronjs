6d385d4f50e4b7b8e11c9759e9ee85ef78d73857
Kaique Covo <kaique.kng@gmail.com>
2022-04-20 22:05:09 -0300

    Função de adicionar item

---
+++ todo-list.js
@@ -0,0 +1,17 @@
+function adicionarItem() {
+  const valorInput = "Minha primeira tarefa";
+
+  // Pegando a tag UL do nosso HTML pelo ID
+  const minhaTagUL = document.getElementById("lista-de-tarefas");
+
+  // Criando tag LI com JavaScript
+  const criarTagLI = document.createElement("li");
+
+  // Adicionando um texto para nossa tag li criada
+  criarTagLI.innerText = valorInput;
+
+  // Adicionando a tag li para nossa ul
+  minhaTagUL.appendChild(criarTagLI);
+
+  console.log("ENTROU NA FUNÇÃO ADICIONAR ITEM");
+}
