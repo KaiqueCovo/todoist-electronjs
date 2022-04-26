4fd745206e0ef93fe4bd969942d6bd5ac2650a70
Kaique Covo <kaique.kng@gmail.com>
2022-04-25 22:33:31 -0300

    Adicionando botão de remover tarefa

--- todo-list.js
+++ todo-list.js
@@ -20,11 +20,29 @@ function adicionarItem() {
   // Criando tag LI com JavaScript
   const criarTagLI = document.createElement("li");
 
+  // Criando uma tag em negrito
+  const tagRemover = "<b onclick='removerItem(event)'> Remover </b>";
+
   // Adicionando um texto para nossa tag li criada
-  criarTagLI.innerText = valorInput;
+  criarTagLI.innerHTML = valorInput + tagRemover;
 
   // Adicionando a tag li para nossa ul
   minhaTagUL.appendChild(criarTagLI);
 
   console.log("ENTROU NA FUNÇÃO ADICIONAR ITEM");
+}
+
+function removerItem(event) {
+  // Pegando tag <b> dentro do nosso evento
+  const tagRemover = event.target;
+
+  const tagLi = tagRemover.parentNode;
+
+  console.log("CLIKEI NA FUNÇÃO REMOVER");
+
+  //tagLi.parentNode.removeChild(node);
+
+  tagLi.remove();
+
+  console.log(tagLi);
 }
