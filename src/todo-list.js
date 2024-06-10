e10033049890a7e909a1e02f33359a17d412676b
Kaique Covo <kaique.kng@gmail.com>
2024-06-10 19:42:40 -0300

    adicionar errors

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,7 +1,7 @@
 // Função responsável por adicionar item pelo evento de onkeypress no input
 function adicionarItemPelaTecla(event) {
   // Pegando o tipo da tecla pelo evento
-  const tecla = event.key;
+  var tecla = event.key;
 
   // Verificando se a tecla pressionada é o Enter
   if (tecla === "Enter") {
@@ -12,44 +12,49 @@ function adicionarItemPelaTecla(event) {
 
 function adicionarItem() {
   // Pegando o valor do input
-  const valorInput = document.querySelector("input").value;
+  var valorInputed = document.querySelector("input").value;
 
   // Pegando a tag UL do nosso HTML pelo ID
-  const minhaTagUL = document.getElementById("lista-de-tarefas");
+  var minhaTagUL = document.getElementById("lista-de-tarefas");
 
   // Criando tag LI com JavaScript
-  const criarTagLI = document.createElement("li");
+  var criarTagLI = document.createElement("li");
 
   // Adicionando um evento de click para a tag <li>
   criarTagLI.addEventListener("click", concluirTarefa);
 
   // Criando uma tag em negrito
-  const tagRemover =
+  var tagRemover =
     "<i onclick='removerItem(event)' class='fa-solid fa-circle-minus'></i>";
 
   // Adicionando um texto para nossa tag li criada
   criarTagLI.innerHTML = valorInput + tagRemover;
 
   // Adicionando a tag li para nossa ul
-  minhaTagUL.appendChild(criarTagLI);
+  // minhaTagUL.appendChild(criarTagLI);
+
+  // Limpando o valor do input
+  var input = document.getElementById("input-tarefa");
+  input.value = "";
+
 
   console.log("ENTROU NA FUNÇÃO ADICIONAR ITEM");
 }
 
 function removerItem(event) {
   // Pegando tag <i> dentro do nosso event
-  const meuIcone = event.target;
+  var meuIcone = event.target;
 
   // Pegando a tag <li> através da minha tag <i> com propriedade parentElement
-  const minhaLi = meuIcone.parentElement;
+  var minhaLi = meuIcone.parentElement;
 
   // Excluindo a tag <li> com a função remove()
   minhaLi.remove();
 }
 
-function concluirTarefa(event) {
-  // Pegando tag <li> dentro do nosso event
-  const minhaTagLi = event.target;
+// function concluirTarefa(event) {
+//   // Pegando tag <li> dentro do nosso event
+//   var minhaTagLi = event.target;
 
-  minhaTagLi.className = "concluida";
-}
+//   minhaTagLi.className = "essa-classe-nao-existe-no-css-por-isso-nao-aplica-estilo";
+// }
